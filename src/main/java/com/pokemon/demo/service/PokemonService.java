a9414c84a2878d55ac9ad71b8452343140d3ef81
FD-FuRy <83590934+FD-FuRy@users.noreply.github.com>
2022-12-01 12:38:08 +0100

    construction de pokemonService
    Construction de la classe pokemonserviceImpl et le l'interface PokemonService

--- src/main/java/com/pokemon/demo/service/PokemonService.java
+++ src/main/java/com/pokemon/demo/service/PokemonService.java
@@ -1,4 +1,20 @@
 package com.pokemon.demo.service;
 
+import com.pokemon.demo.pojo.Pokemon;
+
+import java.util.List;
+
 public interface PokemonService {
+
+    List<Pokemon> getAllPokemon();
+
+    List<Pokemon> getAllPokemonByName(String name);
+
+    void updatePokemon(String pokemonName, Pokemon pokemon);
+
+    void createPokemon(Pokemon pokemon);
+
+    void deletePokemon(String name);
+
+
 }
--- src/main/java/com/pokemon/demo/service/impl/PokemonServiceImpl.java
+++ src/main/java/com/pokemon/demo/service/impl/PokemonServiceImpl.java
@@ -1,4 +1,43 @@
 package com.pokemon.demo.service.impl;
 
-public class PokemonServiceImpl {
+import com.pokemon.demo.pojo.Pokemon;
+import com.pokemon.demo.service.PokemonService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PokemonServiceImpl implements PokemonService {
+
+    private static List<Pokemon> maListe = new ArrayList<>();
+
+    @Override
+    public List<Pokemon> getAllPokemon() {
+        return maListe;
+    }
+
+    @Override
+    public List<Pokemon> getAllPokemonByName(String name) {
+        return maListe.stream()
+                .filter( pokemon -> pokemon.getSurname().equals(name) )
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void updatePokemon(String pokemonName, Pokemon pokemon) {
+        this.deletePokemon(pokemonName);
+        maListe.add(pokemon);
+    }
+
+    @Override
+    public void createPokemon(Pokemon pokemon) {
+        maListe.add(pokemon);
+    }
+
+    @Override
+    public void deletePokemon(String name) {
+        maListe.removeIf( monPokemon -> monPokemon.getSurname().equals(name) );
+    }
 }
